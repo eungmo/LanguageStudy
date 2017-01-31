54fb96fd171bedb532cc85996d9ddf3ddbf09c6a
eungmo <joeseungmo@gmail.com>
2017-01-31 23:15:09 +0800

    2017.01.31 AngularJS Developer Guide Study

---
+++ Angular.js/angular-phonecat/1.2_1/app.spec.js
@@ -0,0 +1,24 @@
+describe('PhoneListController', function() {
+  beforeEach(module('phonecatApp'));
+  
+  it('should create a phones model with 3 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+
+    expect(scope.phones.length).toBe(3);	
+  }));
+  
+  it('should create a phones model with 4 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+	
+	expect(scope.phones.length).toBe(4);
+  }));
+  
+  it('should create a name that having value -would-', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+
+	expect(scope.name).toBe('would');
+  }));
+});
