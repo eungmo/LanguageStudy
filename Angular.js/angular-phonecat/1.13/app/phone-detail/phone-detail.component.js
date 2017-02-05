1cc05e9747d7b6dcde991149d4fcba7382ec198e
eungmo <joeseungmo@gmail.com>
2017-02-06 00:01:40 +0800

    AngularJS Developer Guide Study5

---
+++ Angular.js/angular-phonecat/1.13/app/phone-detail/phone-detail.component.js
@@ -0,0 +1,36 @@
+angular.module('phoneDetail').component('phoneDetail', {
+  templateUrl: 'phone-detail/phone-detail.template.html',
+  controller: ['$routeParams', 'Phone',
+    function PhoneDetailController($routeParams, Phone) {
+      var self = this;
+      self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        self.setImage(phone.images[0]);
+      });
+      
+      self.setImage = function setImage(imageUrl) {
+        self.mainImageUrl = imageUrl;
+      };
+      
+      self.onDblclick = function onDblclick(imageUrl) {
+        alert('You double-clicked image: ' + imageUrl);
+      }
+    }
+    //'$http', '$routeParams', 
+    //function PhoneDetailController($http, $routeParams) {
+    //  var self = this;
+    //  
+    //  self.setImage = function setImage(imageUrl) {
+    //    self.mainImageUrl = imageUrl;
+    //  };
+    //  
+    //  self.onDblclick = function onDblclick(imageUrl) {
+    //    alert('You doulbe-clicked image: ' + imageUrl);
+    //  }
+    //  
+    //  $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+    //    self.phone = response.data;
+    //    self.setImage(self.phone.images[0]);
+    //  });
+    //}
+  ]
+});
